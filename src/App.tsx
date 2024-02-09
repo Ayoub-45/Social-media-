40778f2fda38256faccacf413dd9ef99f9747f5f
Ayoub-45 <ayoubnasraoui161@yahoo.fr>
2024-02-09 08:21:11 +0100

    Setup our project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from "react";
-
+import "./globals.css";
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="text-3xl font-bold underline">
+      <h1>App</h1>
+    </div>
+  );
 }
 
 export default App;
